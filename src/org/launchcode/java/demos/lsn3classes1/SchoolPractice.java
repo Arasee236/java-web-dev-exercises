5e4d4d173a0f5a1c159058e4ae3594bf0cbe3894
Arasi Arumugam <ezhil.arum@gmail.com>
2021-07-28 17:59:00 -0500

    removed unwanted code

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -4,6 +4,6 @@ public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
 
-        Student studentOne = new Student("Arasi",)
+       // Student studentOne = new Student("Arasi",)
     }
 }
--- src/org/launchcode/java/studios/countingCharacters/countChar.java
+++ src/org/launchcode/java/studios/countingCharacters/countChar.java
@@ -7,15 +7,10 @@ public class countChar {
     public static void main(String[] args) throws FileNotFoundException{
 
         Scanner input = new Scanner(System.in);
-//      String passage = "If the product of two terms is zero then common sense says at least one of the two terms has to " +
-//              "be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a " +
-//              "form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, " +
-//              "it’s pretty straightforward from there." ;
-        //System.out.println("Enter the String : ");
-        //String passage = input.nextLine();
+
         String passage = "";
         passage = new Scanner(new File("C:\\Users\\arase\\OneDrive\\Desktop\\coder_girl\\Java\\java-web-dev-exercises\\src\\org\\launchcode\\java\\studios\\datafile.txt")).useDelimiter("\\Z").next();
-       // System.out.println(passage);
+
 
 
         String newPassage = passage.toLowerCase();
@@ -23,14 +18,14 @@ public class countChar {
 
         char[] charactersInString = newPassage.toCharArray();
 
-        //Welcome to coder girl
+
         HashMap<Character, Integer> characters = new HashMap<>();
-        int count = 0;
+
         List<String> t = new ArrayList<>();
 
         for (char letter : charactersInString) {
 
-            //count = countCharacter(charactersInString, letter);
+
             if(characters.containsKey(letter)){
                 Integer charCount = characters.get(letter);
                 characters.put(letter, charCount + 1);
@@ -51,13 +46,5 @@ public class countChar {
 
     }
 
-//    public static int countCharacter(char[] arr, char character) {
-//        int total = 0;
-//        for (int i = 0; i < arr.length; i++) {
-//            if (arr[i] == character) {
-//                total += 1;
-//            }
-//        }
-//        return total;
-//    }
+
 }
